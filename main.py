5180bd78e0f890e1a0c110a68422c24c1cc43f82
maxjoerg <34790296+maxjoerg@users.noreply.github.com>
2022-03-03 13:59:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,24 +29,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET", "OPTIONS", "DELETE", "PUT"],
-    allow_headers=[
-      #  "Access-Control-Allow-Headers",
-        "Origin",
-        "Accept",
-        "X-Requested-With",
-      #  "Content-Type",
-      #  "Access-Control-Request-Method",
-      #  "Access-Control-Request-Headers",
-     #   "Access-Control-Allow-Origin",
-     #   "Access-Control-Allow-Methods"
-     #   "Authorization",
-        "X-Amz-Date",
-        "X-Api-Key",
-        "X-Amz-Security-Token"
-    ]
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
 
