c11aca80d51a8365e7c144b5ecd1d767b088c693
maxjoerg <34790296+maxjoerg@users.noreply.github.com>
2022-03-03 13:54:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,15 +33,15 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["POST", "GET", "OPTIONS", "DELETE", "PUT"],
     allow_headers=[
-        "Access-Control-Allow-Headers",
+      #  "Access-Control-Allow-Headers",
         "Origin",
         "Accept",
         "X-Requested-With",
-        "Content-Type",
+      #  "Content-Type",
         "Access-Control-Request-Method",
-        "Access-Control-Request-Headers",
-        "Access-Control-Allow-Origin",
-        "Access-Control-Allow-Methods"
+      #  "Access-Control-Request-Headers",
+     #   "Access-Control-Allow-Origin",
+     #   "Access-Control-Allow-Methods"
         "Authorization",
         "X-Amz-Date",
         "X-Api-Key",
